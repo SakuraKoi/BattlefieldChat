7043bebc0bf5d0895fffba68bb55110b5f856bdb
SakuraKooi <sakurakoi993519867@gmail.com>
2021-12-11 13:07:28 +0800

    [Enhancement] DPI awareness ( close #14 )

--- BattlefieldChat/main.cpp
+++ BattlefieldChat/main.cpp
@@ -3,7 +3,10 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true); // FIXME: 不完美实现
+
     QApplication a(argc, argv);
+
     BattlefieldChat w;
     w.show();
     return a.exec();
