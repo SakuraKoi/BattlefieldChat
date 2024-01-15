a0a0fcbe60cb7141cd9756203ccf215e28f6eda1
SakuraKooi <me@sakurakooi.dev>
2024-01-15 16:36:32 +0800

    fix: typo

--- BattlefieldChat/Preprocessor.h
+++ BattlefieldChat/Preprocessor.h
@@ -8,7 +8,7 @@ class Preprocessor { // Nop, just convert to utf8 and return
 public:
     virtual std::string process(QString input);
     virtual void initialize(); // called when input dialog shown
-    virtual QString enterPressed(QString input); // return NULL to perform prrocess and send, else replace
+    virtual QString enterPressed(QString input); // return NULL to perform process() and send, else replace
     virtual QString escPressed(QString input); // return NULL to cancel input, else replace
     virtual bool shouldValidateLength();
 };
